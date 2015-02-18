51d43e38e0e4758dcd2ebd258d3f73bd997b3b08
goodmentrans <greenwichmeantime@yahoo.com>
2015-02-18 15:25:50 -0800

    Cleaned up some code structure
    Dialog-action-box still does not work in Chrome or Opera...

--- app/view/contact.view.php
+++ app/view/contact.view.php
@@ -8,94 +8,8 @@
 	<link href="http://fonts.googleapis.com/css?family=Pacifico:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 		
 	<link rel="stylesheet" href="css/style.css">
-				
-	<style>
-	#info-bar {
-		list-style: none;
-		width: 100%;
-		margin: 0;
-		padding: 0 0 0 5px;
-		border-left: 1px solid #d7d7d7;
-	}
-	
-	#main-section {
-		background: #f8f8f8;
-		position: relative;
-		padding-bottom: 40px;
-	}
-	
-	#main-container {
-		position: relative;
-		top: -160px;
-		margin-bottom: -160px;
-	}
-	
-	#static-content {
-		overflow: hidden;
-		background: #fff;
-		border-top: 7px solid #4ca166;
-		border-left: 1px solid #d7d7d7;
-		border-right: 1px solid #d7d7d7;
-		border-bottom: 1px solid #d7d7d7;
-		padding: 30px;
-	}
-	
-	#static-content h1 {
-		margin: 0 0 0;
-		font-size: 2.2em;
-		color: #444;
-		font-weight:400;
-	}
+	<link rel="stylesheet" href="css/static-style.css">
 
-	#static-content h3 {
-		color: #000;
-		font-weight:400;
-		font-family: 'Pacifico';
-	}
-
-	#static-content h4 {
-		color:#666;
-		font-weight:300;
-		font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, Sans Serif;
-	}
-
-	#static-content h5 {
-		color:#666;
-		margin-left:0.2em;
-		font-weight:300;
-		font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, Sans Serif;
-	}
-	
-	#static-document {
-		background: rgba(0, 0, 0, 0.5);
-		padding: 25px;
-		color: #fff;
-	}
-	
-	#static-document h1 {
-		margin: 0 0 0.05em;
-		color: #fff;
-		font-weight: 300;
-		font-size: 2.2em;
-		font-family: 'Pacifico';
-	}
-	
-	#static-document p {
-		margin: 0;
-		font-weight: 300;
-	}
-	
-	#static-document a {
-		color: #fff;
-		text-decoration: underline;
-	}
-	
-	#static-document a:hover {
-		text-decoration: none;
-	}
-	
-	</style>
-	
 	<script src="js/jquery-2.1.1.min.js"></script>
 	<script src="js/skel.min.js"></script>
 	<script src="js/skel-layers.min.js"></script>
@@ -128,39 +42,5 @@
 					We love getting your feedback! Contact us directly by telephone at [phone number] or by email at <a href="mailto:support@trainingful.com">support@trainingful.com</a>.</h4>
 				</p>
 			</div>
-<script>
-
-
-  var scope = document.querySelector('template[is=auto-binding]');
-
-  scope.toggleDialog1 = function(e) {
-    if (e.target.localName != 'button') {
-      return;
-    }
-    var d = e.target.nextElementSibling;
-    if (!d) {
-      return;
-    }
-    d.toggle();
-  };
-
-  scope.transitions = [
-    'core-transition-center',
-    'core-transition-top',
-    'core-transition-bottom',
-    'core-transition-left',
-    'core-transition-right'
-  ];
-
-  scope.toggleDialog2 = function(e) {
-    if (e.target.localName != 'button') {
-      return;
-    }
-    scope.transition = e.target.getAttribute('transition');
-    document.getElementById('dialog2').toggle();
-  };
-
-</script>
-
 		</div>
 	</section>
--- app/view/header.view.php
+++ app/view/header.view.php
@@ -24,172 +24,143 @@
 	#signin-content {
 		overflow: hidden;
 		background: #fff;
-	
 	}
 
-			#signin-content h1 {
-				margin: 0 0 0;
-				font-size: 2.2em;
-				color: #666;
-			}
+	#signin-content h1 {
+		margin: 0 0 0;
+		font-size: 2.2em;
+		color: #666;
+	}
 
-			#signin-content h2 {
-				font-weight:300;
-			}
-			
-			#signin-content h3 {
-				font-weight: 300;
-				font-size: 1.4em;
-			}
+	#signin-content h2 {
+		font-weight:300;
+	}
+	
+	#signin-content h3 {
+		font-weight: 300;
+		font-size: 1.4em;
+	}
 
-			#signin-content h4 {
-				color:#666;
-				font-weight: 400;
-				font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, Sans Serif;
-			}
+	#signin-content h4 {
+		color:#666;
+		font-weight: 400;
+		font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, Sans Serif;
+	}
 
-			#signin-content h5 {
-				color:#666;
-				margin-left: 0.2em;
-				font-weight: 400;
-				font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, Sans Serif;
-			}
+	#signin-content h5 {
+		color:#666;
+		margin-left: 0.2em;
+		font-weight: 400;
+		font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, Sans Serif;
+	}
 
-			#signin-content .recommended {
-				font-size: 0.7em;
-				font-weight: 600;
-				color: #4ca166;
-			}
-			
-			#signin-content .benefits {
-				list-style: none;
-				font-weight: 400;
-				font-size: 0.85em;
-				color: #666;
-			}
+	#signin-content .recommended {
+		font-size: 0.7em;
+		font-weight: 600;
+		color: #4ca166;
+	}
+	
+	#signin-content .benefits {
+		list-style: none;
+		font-weight: 400;
+		font-size: 0.85em;
+		color: #666;
+	}
 
-			#signin-content .benefits li:before {
-				content:"- ";
-			}
-			
-			#signin-content .benefits li {
-				margin-top: 2px;
-			}
+	#signin-content .benefits li:before {
+		content:"- ";
+	}
+	
+	#signin-content .benefits li {
+		margin-top: 2px;
+	}
 
-			#signin-content .button-other {
-				height: 40px;
-				width: 250px;
-				text-align: left;
-				border-radius: 3px;
-				border: 0;
-				color: #fff;
-				font-size: 1.2em;
-				box-shadow: 1px 1px 5px #888888;
-			}
+	#signin-content .button-other {
+		height: 40px;
+		width: 250px;
+		text-align: left;
+		border-radius: 3px;
+		border: 0;
+		color: #fff;
+		font-size: 1.2em;
+		box-shadow: 1px 1px 5px #888888;
+	}
 
-			#signin-content .other-linkedin {
-				background: #007bb6;
-			}
-			#signin-content .other-linkedin:hover {
-				background: #1086be;
-			}
-			#signin-content .other-facebook {
-				background: #3b579d;
-			}
-			#signin-content .other-facebook:hover {
-				background: #4c68af;
-			}
+	#signin-content .other-linkedin {
+		background: #007bb6;
+	}
+	#signin-content .other-linkedin:hover {
+		background: #1086be;
+	}
+	#signin-content .other-facebook {
+		background: #3b579d;
+	}
+	#signin-content .other-facebook:hover {
+		background: #4c68af;
+	}
 
-			#signin-content .other-google {
-				background: #dd4b39;
-			}
-			#signin-content .other-google:hover {
-				background: #ef5f4d;
-			}
+	#signin-content .other-google {
+		background: #dd4b39;
+	}
+	#signin-content .other-google:hover {
+		background: #ef5f4d;
+	}
 
-			#signin-content .other-twitter {
-				background: #00b0ed;
-			}
-			#signin-content .other-twitter:hover {
-				background: #14bffb;
-			}
+	#signin-content .other-twitter {
+		background: #00b0ed;
+	}
+	#signin-content .other-twitter:hover {
+		background: #14bffb;
+	}
 
-			#signin-content .other-button-icon {
-				padding-top: 20px;
-				height: 40px;
-				width: 30px;
-				border-right: 1px solid rgba(0, 0, 0, .05);
-				float:left;
-				margin-left: 5px;
-			}
+	#signin-content .other-button-icon {
+		padding-top: 20px;
+		height: 40px;
+		width: 30px;
+		border-right: 1px solid rgba(0, 0, 0, .05);
+		float:left;
+		margin-left: 5px;
+	}
 
-			#signin-content .sign-up-other {
-				font-family: 'Lato', 'Helvetica Neue', Helvetica, Arial, Sans Serif;
-				font-size: 0.8em;
-				font-weight: 300;
-				margin: 10px 0px 0px 50px;
-			}
+	#signin-content .sign-up-other {
+		font-family: 'Lato', 'Helvetica Neue', Helvetica, Arial, Sans Serif;
+		font-size: 0.8em;
+		font-weight: 300;
+		margin: 10px 0px 0px 50px;
+	}
 
-			#signin-content .button-linkedin {
-				margin: 20px 0px 50px 80px;
-				height: 60px;
-				width: 350px;
-				text-align: left;
-				background: #007bb6;
-				border-radius: 3px;
-				border: 0;
-				color: #fff;
-				font-size: 2em;
-				box-shadow: 1px 1px 5px #888888;
-			}
+	#signin-content .button-linkedin {
+		margin: 20px 0px 50px 80px;
+		height: 60px;
+		width: 350px;
+		text-align: left;
+		background: #007bb6;
+		border-radius: 3px;
+		border: 0;
+		color: #fff;
+		font-size: 2em;
+		box-shadow: 1px 1px 5px #888888;
+	}
 
-			#signin-content .button-linkedin:hover {
-				background: #1086be;
-			}
-			
-			#signin-content .linkedin-button-icon {
-				padding-top: 30px;
-				height: 60px;
-				width: 50px;
-				border-right: 1px solid rgba(0, 0, 0, .05);
-				float:left;
-				margin-left: 5px;
-			}
-			
-			#signin-content .sign-up-linkedin {
-				font-family: 'Lato', 'Helvetica Neue', Helvetica, Arial, Sans Serif;
-				font-size: 0.7em;
-				font-weight: 300;
-				margin: 15px 0px 0px 90px;
-			}
-			
-			#static-document {
-				background: rgba(0, 0, 0, 0.5);
-				color: #fff;
-			}
-			
-			#static-document h1 {
-				margin: 0 0 0.05em;
-				color: #fff;
-				font-weight: 300;
-				font-size: 2.2em;
-				font-family: 'Pacifico';
-			}
-			
-			#static-document p {
-				margin: 0;
-				font-weight: 300;
-			}
-			
-			#static-document a {
-				color: #fff;
-				text-decoration: underline;
-			}
-			
-			#static-document a:hover {
-				text-decoration: none;
-			}
-
+	#signin-content .button-linkedin:hover {
+		background: #1086be;
+	}
+	
+	#signin-content .linkedin-button-icon {
+		padding-top: 30px;
+		height: 60px;
+		width: 50px;
+		border-right: 1px solid rgba(0, 0, 0, .05);
+		float:left;
+		margin-left: 5px;
+	}
+	
+	#signin-content .sign-up-linkedin {
+		font-family: 'Lato', 'Helvetica Neue', Helvetica, Arial, Sans Serif;
+		font-size: 0.7em;
+		font-weight: 300;
+		margin: 15px 0px 0px 90px;
+	}
 		</style>
 
 
@@ -207,7 +178,8 @@
 			</ul>
 			<a href="/" id="header-logo"><img src="images/header-logo.png" alt="Trainingful"></a>
 		</div>
-			
+
+
 			<paper-action-dialog backdrop id="review-dialog" heading="Write a Review"
 						  transition="paper-dialog-transition-bottom" style="display:none;">
 				<div id="signin-content">
@@ -229,20 +201,20 @@
 					</div>
 				</div>
 				<paper-button dismissive>Cancel</paper-button>
-				<paper-button affirmative default>SUBMIT YOUR REVIEW</paper-button>
+				<paper-button affirmative autofocus>SUBMIT YOUR REVIEW</paper-button>
 			</paper-action-dialog>
 
 			<paper-action-dialog backdrop id="si-dialog" heading="Sign In"
-						  transition="paper-dialog-transition-bottom" style="display:none;">
-				<div id="signin-content">
-					<div class="row 25% uniform">
-						<div class="5u" style="border-right:1px solid #d7d7d7;padding-bottom:20px;margin-left:20px;margin-top:20px;padding-right:205px;">
+						  transition="paper-dialog-transition-bottom" style="display:none;">S1
+				<div id="signin-content">S2
+					<div class="row 25% uniform">S3
+						<div class="5u" style="border-right:1px solid #d7d7d7;padding-bottom:20px;margin-left:20px;margin-top:20px;padding-right:205px;">S4
 							<p><button type="submit" class="button-other other-linkedin"><div class="icon linkedin other-button-icon"></div><div class="sign-up-other">Sign In with LinkedIn</div></button></p>
 							<p><button type="submit" class="button-other other-facebook"><div class="icon facebook other-button-icon"></div><div class="sign-up-other">Sign In with Facebook</div></button></p>
 							<p><button type="submit" class="button-other other-google"><div class="icon google other-button-icon"></div><div class="sign-up-other">Sign In with Google</div></button></p>
 							<p><button type="submit" class="button-other other-twitter"><div class="icon twitter other-button-icon"></div><div class="sign-up-other">Sign In with Twitter</div></button></p>
 						</div>
-						<div class="5u" style="padding-left:40px;">
+						<div class="5u" style="padding-left:40px;">S5
 							<p><h4>Or sign in with your email.</h4></p>
 							<!--needs a box to enter email address and a box to enter password -->
 						</div>
