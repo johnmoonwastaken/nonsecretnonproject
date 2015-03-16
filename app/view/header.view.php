b8ab4c09cecc4d173dc3108ce2f45f8576077547
goodmentrans <greenwichmeantime@yahoo.com>
2015-03-15 23:04:41 -0700

    minor wording

--- app/view/header.view.php
+++ app/view/header.view.php
@@ -33,7 +33,7 @@
 				<li><a href="#" onClick="document.getElementById('review-dialog').toggle();">Write a Review</a></li>
 				-->
 				<?php if(!isset($_COOKIE['trainingful_oauth'])): ?>
-				<li><a href="signin">Register Vendor</a></li>
+				<li><a href="signin">Register / Sign In</a></li>
 				<?php else: ?>
 				<li><a href="#"><?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?></a></li>
 				<li><a href="signout">Sign Out</a></li>
