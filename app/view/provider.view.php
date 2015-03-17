7c5adead1bdb9b07c8223ad92e0ea11d6b74366a
johnmoonwastaken <johnmoon@gmail.com>
2015-03-17 14:50:06 -0700

    Updated providers page

--- app/view/provider.view.php
+++ app/view/provider.view.php
@@ -25,10 +25,10 @@
 						<br />
 						<p>Why list on Trainingful?
 						<br /><br />
-						<p style="margin-left: 100px; margin-right: 100px">1. <b>List your courses for free.</b> You only pay a small percentage fee, per course actually booked through Trainingful. 
-						<p style="margin-left: 100px; margin-right: 100px">2. <b>Be recognized for your excellence.</b> We encourage past course participants to leave meaningful reviews of your course on Trainingful through their LinkedIn profiles, so that other potential customers can see what sets your courses apart from your competitors’. 
-						<p style="margin-left: 100px; margin-right: 100px">3. <b>Manage your online image.</b> We’ll work closely with you to present your company and courses in the right way.
-						<p style="margin-left: 100px; margin-right: 100px">4. <b>Focus on teaching, not marketing.</b> Customers search Trainingful for the courses they want, when they’re ready to book. Listing on Trainingful means that you have access to this customer base at the right time, so that you can spend more time and effort making your courses better, rather than on marketing.
+						<p style="margin-left: 100px; margin-right: 100px"><strong>1. List your courses for free.</strong> You only pay a small percentage fee, per course actually booked through Trainingful. 
+						<p style="margin-left: 100px; margin-right: 100px"><strong>2. Be recognized for your excellence.</strong> We encourage past course participants to leave meaningful reviews of your course on Trainingful through their LinkedIn profiles, so that other potential customers can see what sets your courses apart from your competitors’. 
+						<p style="margin-left: 100px; margin-right: 100px"><strong>3. Manage your online image.</strong> We’ll work closely with you to present your company and courses in the right way.
+						<p style="margin-left: 100px; margin-right: 100px"><strong>4. Focus on teaching, not marketing.</strong> Customers search Trainingful for the courses they want, when they’re ready to book. Listing on Trainingful means that you have access to this customer base at the right time, so that you can spend more time and effort making your courses better, rather than on marketing.
 						<br /><br />
 						<p>Have more questions? Ready to sign up? <a href="mailto:support@trainingful.com">Contact us</a> today to set up your training provider account. It’s so easy, you’ll wonder why you hadn’t done it earlier.
 			</div>
