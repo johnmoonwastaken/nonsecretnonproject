7b6bfc662c02e48485f14459cd24ecc217cad61d
Renbo <greenwichmeantime@yahoo.com>
2015-03-17 17:23:55 -0700

    Minor changes to header

--- app/view/header.view.php
+++ app/view/header.view.php
@@ -35,7 +35,7 @@
 				<?php if(!isset($_COOKIE['trainingful_oauth'])): ?>
 				<li><a href="signin">Register / Sign In</a></li>
 				<?php elseif ($_SERVER['REQUEST_URI'] != "/signin"): ?>
-				<li><a href="<?php if ($_SESSION['vendor_id'] > 0) { echo 'manage_vendor';} else { echo 'edit_profile';} ?>"><strong><?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?></strong></a></li>
+				<li><a href="<?php if ($_SESSION['vendor_id'] > 0) { echo 'manage_courses';} else { echo 'edit_profile';} ?>"><strong><?php echo $_SESSION['first_name'] . ' ' . $_SESSION['last_name']; ?></strong></a></li>
 				<li><a href="signout">Sign Out</a></li>
 				<?php endif ?>
 				<!--
