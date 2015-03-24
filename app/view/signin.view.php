38a2f119ed276ec18ace70537017d65d87ce9bf2
johnmoonwastaken <johnmoon@gmail.com>
2015-03-23 21:34:52 -0700

    more vendor pics

--- app/view/signin.view.php
+++ app/view/signin.view.php
@@ -11,7 +11,7 @@ $_SESSION['state'] = $state;
 <head>
 	<link rel="import" href="../../bower_components/paper-toast/paper-toast.html">
 <?php include 'header_required.php' ?>
-	<title>trainingful: sign in</title>
+	<title>trainingful: log in</title>
 	
 	<link href="http://fonts.googleapis.com/css?family=Pacifico:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="css/signup.css">
@@ -34,7 +34,7 @@ $_SESSION['state'] = $state;
 	<section id="main-section">
 		<div class="container" id="main-container">
 			<div id="signup-content">
-				<p><h1><span style="font-family: Pacifico;">Sign in or sign up for trainingful</span></h1></p>
+				<p><h1><span style="font-family: Pacifico;">Log in to trainingful</span></h1></p>
 				<div class="row 25% uniform" style="margin-top:20px;">
 					<div class="7u" style="border-right:1px solid #d7d7d7;">
 						<p><h3>Use your LinkedIn account <span class="recommended">(Recommended)</span></h3></p>
