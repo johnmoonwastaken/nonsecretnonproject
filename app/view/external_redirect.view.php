f5c8bdc2327ddc1b4b1ea9dd70749d5b7aea0cf4
goodmentrans <greenwichmeantime@yahoo.com>
2015-04-21 21:35:31 -0700

    Redirect Record Also Adds IP Address

--- app/view/external_redirect.view.php
+++ app/view/external_redirect.view.php
@@ -5,11 +5,11 @@ session_start();
 $url = $_GET['url'];
 $session_id = $_GET['session_id'];
 
-$insert_sql = 'INSERT INTO redirect_record (session_id, redirect_url, timestamp) 
-	VALUES (?,?,now())';
+$insert_sql = 'INSERT INTO redirect_record (session_id, redirect_url, timestamp, ip_address) 
+	VALUES (?,?,now(),?)';
 
 $get_results = $GLOBALS['_db']->prepare($insert_sql);
-$get_results->execute(array($session_id, $url));
+$get_results->execute(array($session_id, $url, $_SERVER['REMOTE_ADDR']));
 
 header('Location: '.$url);
 die();
