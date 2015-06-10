70a91782ccfc77ccf846669e16808f5fddd3afe3
goodmentrans <greenwichmeantime@yahoo.com>
2015-06-09 20:22:25 -0700

    Updated trainingful guarantee to not process null requests

--- app/view/guarantee.view.php
+++ app/view/guarantee.view.php
@@ -6,17 +6,20 @@
 	$ip_address = $_POST['ip_address'];
 	$query_string = $_POST['query_string'];
 
-	$add_sql = "INSERT INTO guarantee (email, name, comments, ip_address, query_string, time) VALUES (?,?,?,?,?,now());";
-	$get_results = $GLOBALS['_db']->prepare($add_sql);
-	$get_results->execute(array($email,$name,$comments,$ip_address,$query_string));
+	if (isset($email)) {
+		$add_sql = "INSERT INTO guarantee (email, name, comments, ip_address, query_string, time) VALUES (?,?,?,?,?,now());";
+		$get_results = $GLOBALS['_db']->prepare($add_sql);
+		$get_results->execute(array($email,$name,$comments,$ip_address,$query_string));
 
-	$mail_message = "E-mail: " . $email . "\nName: " . $name . "\nComments: " . $comments . "\nQuery String: " . $query_string;
+		$mail_message = "E-mail: " . $email . "\nName: " . $name . "\nComments: " . $comments . "\nQuery String: " . $query_string;
 
-	$to = "support@trainingful.com";
-	$subject = "The Trainingful Guarantee";
-	mail($to, $subject, $mail_message);
+		$to = "support@trainingful.com";
+		$subject = "The Trainingful Guarantee";
+		mail($to, $subject, $mail_message);
 
-	header("Location: /trainingful_guarantee?return=sent");
+		header("Location: /trainingful_guarantee?return=sent");
+	}
+
 	die();
 
 ?>
