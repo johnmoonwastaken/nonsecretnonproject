b0a9354b1b76e91ce7776e823cc8fa70042a3499
goodmentrans <greenwichmeantime@yahoo.com>
2015-07-21 01:08:23 -0700

    Added job posting

--- app/view/careers.view.php
+++ app/view/careers.view.php
@@ -28,8 +28,48 @@
 					<h4>We at Trainingful want incredibly talented people (like you!) to join us in connecting professionals and businesses with exciting training opportunities. As a service for professionals, Trainingful requires a broad range of technical and business skills. 
 						<br></br>
 						We believe in results and productivity in a collaborative setting. We value innovation, creativity and diversity. 
-						<br></br>
-						Interested? Have any questions? Send us an <a href="mailto:careers@trainingful.com">email</a> and let's start talking!
+					</h4>
+					<h2>Current Openings:</h2>
+
+					<h5>
+						<a href="internshipjoblisting.pdf">(Printable PDF Job Posting)</a><br /><br />
+						<strong>Job Title:</strong> Student Intern (Unpaid)<br />
+						<strong>Posting Close Date:</strong> Wednesday, July 29th, 2015<br />
+						<strong>Start Date:</strong> ASAP or August 3, 2015<br /><br />
+						We are hiring!<br /><br />
+						Trainingful.com is currently hiring 2-3 generalist summer interns. Hours can be flexible to your schedule but we do expect you to contribute 25-40 hours a week. This position can also be extended into the school year on a part-time basis.   <br /><br />
+						Our team includes a former US lawyer, an investment banker, and an international MBA grad building the world's largest aggregation site for professional training. We are looking for a motivated, driven business student (ideally finance, accounting, or marketing but we will consider everyone) who is able to wear many hats and help drive growth. We are focused on mentoring and developing student interns who can succeed at our company or other companies in the future.   <br /><br />
+						<strong>Your main focus is to drive growth - this includes: </strong><br />
+						<ul>
+							<li>Product development and broadening our product offerings (unfortunately, this sometimes means grunt work such as data entry alongside our founders)</li>
+							<li>Develop career / position certification roadmaps for various potential users</li>
+							<li>Grow size and engagement across social platforms such as Facebook, LinkedIn, Twitter, blogging etc</li>
+							<li>To encourage professional development, we typically ask our interns to spend 10-25% of their time on developing their own growth project (for example, video campaign, partnerships, other growth ideas) - top projects may be selected and executed on a company-wide basis</li>
+						</ul>
+						<strong>Requirements:</strong>
+						<ul>
+							<li>Self-starter and independent worker</li>
+							<li>Entrepreneurial and eager to learn</li>
+							<li>Able to manage time in a fast-paced environment</li>
+							<li>Strong communication skills, both written and verbal</li>
+						</ul>
+						<strong>Bonus Assets:</strong>
+						<ul>
+							<li>Proficient in online digital social media</li>
+							<li>Analytical and data-driven</li>
+							<li>Strong attention to detail</li>
+						</ul>
+
+						As we are currently running this company as lean as we can, please note that we are only able to offer unpaid internships at this time. This position will be largely remote (telecommute) with free food at strategy meetings every 2-4 weeks.<br /><br />
+						<strong>To apply, please send your resume to tina@trainingful.com along with your answers to the following 2 questions (400 words max for both answers):</strong>
+						<ul>
+							<li>What do you think of our product?</li>
+							<li>How do you want this internship contribute to your long-term career goals?</li> 
+						</ul>
+						<strong>Additionally, tell us why we should hire you within one tweet (i.e. in 140 or fewer characters.)</strong>
+					</h5>
+					<h4>
+						Interested? Have any questions? E-mail us at <a href="mailto:careers@trainingful.com">careers@trainingful.com</a> and let's start talking.
 					</h4>
 				</p>
 			</div>
