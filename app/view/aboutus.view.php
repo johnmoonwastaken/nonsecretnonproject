8fae8bfd0a82b43c9a354c09dd68f8f3d72623c1
goodmentrans <greenwichmeantime@yahoo.com>
2015-09-20 20:08:25 -0700

    Updated all key pages to responsive

--- app/view/aboutus.view.php
+++ app/view/aboutus.view.php
@@ -15,11 +15,9 @@
 				</div>
 			</section>
 		</div>
-		<div class="container">
-			<div id="static-content">
-			<p><h4>As professionals, we found that we (and our colleagues and friends) often signed up for professional development courses based solely on marketing information from the training providers. We found it difficult to even find all the courses offered in our cities. It was a haphazard way to find information about something so important for our careers.<br /><br />
+		<div id="static-content">
+		<p><h4>As professionals, we found that we (and our colleagues and friends) often signed up for professional development courses based solely on marketing information from the training providers. We found it difficult to even find all the courses offered in our cities. It was a haphazard way to find information about something so important for our careers.<br /><br />
 We knew there had to be a better way.<br /><br />
 Trainingful was conceived in 2014 with a simple mission: to help professionals find meaningful information about training and professional development. <br /><br />
 We work with training providers and conduct continual independent research to present busy professionals with a comprehensive, easy-to-search database of all relevant training courses.</h4></p>
-			</div>
 		</div>
